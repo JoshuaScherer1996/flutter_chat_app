872c1c32151cbaf1e00783b046d6b1088083d3fb
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-02-17 12:23:35 +0100

    Started setting up the chat screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_app/screens/auth.dart';
+import 'package:flutter_chat_app/screens/auth_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
